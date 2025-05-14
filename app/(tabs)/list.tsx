f98f07827f0abc74acd92cf280b7221e39a85be7
gman2004-spec <Viniciusbragastrujak@gmail.com>
2025-05-14 16:48:28 -0300

    Update list.tsx

--- app/(tabs)/list.tsx
+++ app/(tabs)/list.tsx
@@ -1,10 +1,42 @@
-
-import { Text } from "react-native"
+import { Text, View, StyleSheet, SafeAreaView, FlatList } from "react-native"
+import data  from "@/components/constants/data"
+import { Item } from "@/components/item"
 
 export default function List(){
     return(
         <>
-            <Text>Lista</Text>
+            <View style={styles.center}>
+                <Text>Lista</Text>
+            </View>
+            <SafeAreaView style={styles.container}>
+                <FlatList
+                 style={styles.back}
+                 data={data}
+                 renderItem={({item}) => (
+                    <Item name={item.nome} age={item.idade} date={item.data} image={item.imagem}/>
+                )}
+                 keyExtractor={(item) => item.id}
+                />
+            </SafeAreaView>
         </>
     )
 }
+
+const styles = StyleSheet.create({
+    center: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        shadowColor: 'black',
+        shadowOpacity: 0.26,
+        shadowOffset: {width: 0, height: 2},
+        shadowRadius: 10,
+        paddingVertical: 20
+    },
+    container: {
+        flex: 1,
+    },
+    back: {
+        paddingHorizontal: 20
+    }
+})
